ec88ed76efcc15f5af5bda277e6ce4996c733dee
jihwanTW <30304927+jihwanTW@users.noreply.github.com>
2017-12-29 16:42:44 +0900

    chagne

--- src/emq_auth_custom_redis.erl
+++ src/emq_auth_custom_redis.erl
@@ -49,7 +49,9 @@ on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId,username
     case ClientId of
         <<"MQTT_TEMP">>->
             Client = emqttd_cm:lookup(ClientId),
-            emqttd_session:subscribe(Client#mqtt_client.client_pid,[{<<"tempBoard">>,[{qos,0}]}]);
+
+            emqttd_client:subscribe(Client#mqtt_client.client_pid);
+            %emqttd_session:subscribe(Client#mqtt_client.client_pid,[{<<"tempBoard">>,[{qos,0}]}]);
         _->
             undefined
     end,
